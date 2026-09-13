eae279447dda66bfe4aefef624f9ec218b7d64c4
Shravani-jinjore <shrawanij9696@gmail.com>
2026-09-13 22:04:46 +0530

    restore

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -44,7 +44,7 @@ router.post("/", (req, res) => {
       return res.status(500).json({ message: "Database error" });
     }
 
-    res.status(200).json({
+    res.status(201).json({
       id: result.insertId,
       name,
       email,
